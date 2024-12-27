597f125642e24b276f4419f7d03047375001d77c
Maksim Akimov <akimovmax@mail.ru>
2024-12-27 15:58:34 +0300

    Проверка деления на 0

--- 3-calc-events/actions/divide.js
+++ 3-calc-events/actions/divide.js
@@ -1,3 +1,11 @@
-const divide = (a, b) => console.log(`${a} / ${b} = ${a / b}`);
+const divide = (a, b) => {
+	switch (b) {
+		case 0:
+			console.log(`${a} / ${b} = ထ`);
+			break;
+		default:
+			console.log(`${a} / ${b} = ${a / b}`);
+	}
+};
 
 module.exports = divide;
