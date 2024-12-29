d6e5cb16837b73bd26e3efec00aabe88fb951a58
Maksim Akimov <akimovmax@mail.ru>
2024-12-29 11:20:20 +0300

    Добавлена валидация вводимых значений

--- 4-timer/index.js
+++ 4-timer/index.js
@@ -1,28 +1,10 @@
 const { argv } = require('node:process');
-const {parseTime, getTimeInMiliseconds, getHumanLikeTime} = require('./util/util');
+const { validate, startTimer, printErrors } = require('./util/util');
 
-const INTERVAL_TIME = 1000;
+const { isOkay, payload, errMessages } = validate(argv[2],argv[3],argv[4]);
 
-const startTimer = (ms) => {
-	console.log(`⏰ [Таймер] Запущен: ${getHumanLikeTime(ms)}`);
-
-	const intervalId = setInterval(() => {
-		const remainingTime = Math.round((ms - performance.now()));
-		console.log(`⏰ [Таймер] Осталось: ${getHumanLikeTime(remainingTime)}`)
-	}, INTERVAL_TIME);
-
-	setTimeout(() => {
-		clearInterval(intervalId);
-		console.log('⏰ [Таймер] Время истекло.')
-	}, ms);
+if (isOkay) {
+	startTimer(payload);
+} else {
+	printErrors(errMessages);
 }
-
-const parsedTime = parseTime([
-	argv[2],
-	argv[3],
-	argv[4]
-]);
-
-const timeInMiliseconds = getTimeInMiliseconds(parsedTime);
-
-startTimer(timeInMiliseconds);
--- 4-timer/util/util.js
+++ 4-timer/util/util.js
@@ -1,26 +1,54 @@
-const MAX_HOUR = 24;
-const MAX_MINUTE = 60;
-const MAX_SECOND = 60;
+const Hour = { MIN: 0, MAX: 23 };
+const Minute = { MIN: 0, MAX: 59 };
+const Second = { MIN: 0, MAX: 59 };
 
-const DEFAULT_HOUR = 0;
-const DEFAULT_MINUTE = 0;
-const DEFAULT_SECOND = 0;
+const INTERVAL_TIME = 1000;
 
+const isNumber = (value) => {
+	if (typeof value === 'number' && !Number.isNaN(value)) {
+		return true;
+	}
+	return false;
+};
 
-const parseTime = ([h, m, s]) => {
-	const hour = Number(h) || DEFAULT_HOUR;
-	const minute = Number(m) || DEFAULT_MINUTE;
-	const second = Number(s) || DEFAULT_SECOND;
+const isValid = (value, unitMin, unitMax) => {
+	if (!isNumber(value) || value < unitMin || value > unitMax) {
+		return false;
+	}
+	return true;
+};
 
-	return [
-		hour >= MAX_HOUR ? DEFAULT_HOUR : hour,
-		minute >= MAX_MINUTE ? DEFAULT_MINUTE : minute,
-		second >= MAX_SECOND ? DEFAULT_SECOND : second,
-	];
-}
+const validate = (argv2, argv3, argv4) => {
+	console.log([argv2, argv3, argv4])
+	const payload = {
+		hours: Number(argv2),
+		minutes: Number(argv3),
+		seconds: Number(argv4),
+	};
+	console.log(payload)
+	const errMessages = [];
 
-const getTimeInMiliseconds = ([h, m, s]) => {
-	return 1000 * (h * 3600 + m * 60 + s);
+	if (!isValid(payload.hours, Hour.MIN, Hour.MAX)) {
+		errMessages.push(`Количество часов '${argv2}' должно быть числом в диапазоне от ${Hour.MIN} до ${Hour.MAX}.`);
+	}
+
+	if (!isValid(payload.minutes, Minute.MIN, Minute.MAX)) {
+		errMessages.push(`Количество минут '${argv3}' должно быть числом в диапазоне от ${Minute.MIN} до ${Minute.MAX}.`);
+	}
+
+	if (!isValid(payload.seconds, Second.MIN, Second.MAX)) {
+		errMessages.push(`Количество секунд '${argv4}' должно быть числом в диапазоне от ${Second.MIN} до ${Second.MAX}.`);
+	}
+
+	if (errMessages.length > 0) {
+		return { isOkay: false, payload: {}, errMessages };
+	}
+
+	return { isOkay: true, payload, errMessages };
+};
+
+const getTimeInMiliseconds = ({ hours, minutes, seconds }) => {
+	return 1000 * (hours * 3600 + minutes * 60 + seconds);
 };
 
 const getHumanLikeTime = (ms) => {
@@ -28,6 +56,29 @@ const getHumanLikeTime = (ms) => {
 	const minute = Math.floor(((ms / 1000) - (hour * 3600)) / 60);
 	const second = Math.round((ms / 1000) - (hour * 3600) - (minute * 60));
 	return `${hour} ч ${minute} мин ${second} сек`;
-}
+};
 
-module.exports = {parseTime, getTimeInMiliseconds, getHumanLikeTime};
+const startTimer = ({ hours, minutes, seconds }) => {
+	const ms = getTimeInMiliseconds({ hours, minutes, seconds });
+	console.log(`⏰ [Таймер] Запущен: ${getHumanLikeTime(ms)}`);
+
+	const intervalId = setInterval(() => {
+		const remainingTime = Math.round((ms - performance.now()));
+		console.log(`⏰ [Таймер] Осталось: ${getHumanLikeTime(remainingTime)}`)
+	}, INTERVAL_TIME);
+
+	setTimeout(() => {
+		clearInterval(intervalId);
+		console.log('⏰ [Таймер] Время истекло.')
+	}, ms);
+};
+
+const printErrors = (errMessages) => {
+	console.log('Невалидные данные:');
+	console.log(`${errMessages.join('\n')}`);
+	console.log('-----');
+	console.log('Пример валидного ввода:');
+	console.log('>node index.js 0 0 10');
+};
+
+module.exports = { validate, startTimer, printErrors };
