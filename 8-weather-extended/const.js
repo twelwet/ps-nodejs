3f923a4f1fa6472ff5dfc7a5ed616ae735cd03b0
Maksim <akimovmax@mail.ru>
2025-02-21 16:19:23 +0300

    Удалил не нужное

--- 8-weather-extended/const.js
+++ 8-weather-extended/const.js
@@ -26,29 +26,6 @@ const DATA_FIELD = {
 	CITIES: 'cities',
 };
 
-const getIcon = (icon) => {
-	switch (icon.slice(0, -1)) {
-		case '01':
-			return '☀️';
-		case '02':
-			return '🌤️';
-		case '03':
-			return '☁️';
-		case '04':
-			return '☁️';
-		case '09':
-			return '🌧️';
-		case '10':
-			return '🌦️';
-		case '11':
-			return '🌩️';
-		case '13':
-			return '❄️';
-		case '50':
-			return '🌫️';
-	}
-};
-
 const ICONS = new Map([
 	['01', '☀️'],
 	['02', '🌤️'],
