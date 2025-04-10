36526675e23f9f1c3b4b8dd9430519d2ef8e844f
Maksim <akimovmax@mail.ru>
2025-04-10 17:14:07 +0300

    Создан Exeption Filter

--- 18-api-example/src/app.ts
+++ 18-api-example/src/app.ts
@@ -2,6 +2,7 @@ import { Server } from 'node:http';
 import express, { Express } from 'express';
 import { UserController } from './users/user.controller';
 import { LoggerService } from './logger/logger.service';
+import { ExeptionFilter } from './errors/exeption.filter';
 
 export class App {
 	app: Express;
@@ -9,20 +10,31 @@ export class App {
 	port: number;
 	logger: LoggerService;
 	userController: UserController;
+	exeptionFilter: ExeptionFilter;
 
-	constructor(logger: LoggerService, userController: UserController) {
+	constructor(
+		logger: LoggerService,
+		userController: UserController,
+		exeptionFilter: ExeptionFilter,
+	) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
 		this.userController = userController;
+		this.exeptionFilter = exeptionFilter;
 	}
 
 	useRoutes() {
 		this.app.use('/users', this.userController.router);
 	}
 
+	useExeptionFilters() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+	}
+
 	public async init() {
 		this.useRoutes();
+		this.useExeptionFilters();
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Сервер запущен на localhost:${this.port}`);
 	}
--- 18-api-example/src/users/user.controller.ts
+++ 18-api-example/src/users/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { BaseController } from '../common/base.controller';
 import { LoggerService } from '../logger/logger.service';
+import { HTTPError } from '../errors/http-error.class';
 
 export class UserController extends BaseController {
 	constructor(logger: LoggerService) {
@@ -12,7 +13,7 @@ export class UserController extends BaseController {
 	}
 
 	login(req: Request, res: Response, next: NextFunction) {
-		this.ok(res, 'login');
+		next(new HTTPError(401, 'Ошибка авторизации', 'login'));
 	}
 
 	register(req: Request, res: Response, next: NextFunction) {
