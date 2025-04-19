50da39ee7c5de331c2b85fb22836e5dd16eef683
Maksim <akimovmax@mail.ru>
2025-04-19 15:40:27 +0300

    Теперь AuthMiddleware работает корректно

--- 18-api-example/src/common/auth.middleware.ts
+++ 18-api-example/src/common/auth.middleware.ts
@@ -1,7 +1,6 @@
 import { verify } from 'jsonwebtoken';
 import { IMiddleware } from './middleware.interface';
 import { Request, Response, NextFunction } from 'express';
-import { JwtPayload } from 'jsonwebtoken';
 
 export class AuthMiddleware implements IMiddleware {
 	constructor(private secret: string) {}
@@ -17,7 +16,8 @@ export class AuthMiddleware implements IMiddleware {
 					next();
 				}
 			});
+		} else {
+			next();
 		}
-		next();
 	}
 }
